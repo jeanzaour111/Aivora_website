7967717700b677b219dff93a9ec0ea3a47d90ba5
jeanzaour111 <jeanzaour338@gmail.com>
2026-08-25 14:26:53 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,11 @@
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a8c1acaf4f9c83445ed7341/1k0pkh9v8';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
